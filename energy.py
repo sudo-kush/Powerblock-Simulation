5148672e41635f7c1f78f1f90f2266fa8ade04d3
Caleb Menjivar <54721971+sudo-kush@users.noreply.github.com>
2020-04-08 14:57:02 -0700

    Add files via upload

--- energy.py
+++ energy.py
@@ -7,20 +7,35 @@ Created on Wed Apr  8 12:47:15 2020
 import numpy as np 
 import matplotlib.pyplot as plt 
 import math 
-import storage 
+import power 
 
 #375,225
 '''
 July Energy Grid Demand for a 300MWe 
 '''
-
+massflow = 272.2 #kg/s
+tolerence = 5.0
 x0 = np.linspace(0,24,101)
 x1 = np.linspace(0,2*math.pi,101)
 x2 = np.linspace(.6,1,101)
 y0 = np.zeros(101)
+y1 = np.zeros(101)
 
 for i in range(len(y0)):
     y0[i] = -75*math.sin(x1[i]) + 300 
-    
+      
 plt.plot(x0,y0)
 
+for j in range(len(y0)):
+    for k in range(len(y0)):
+        if ((power.CycleP(x2[k],massflow)) < tolerence):
+            y1[j] = x2[k] 
+            
+            
+            
+            
+    
+ 
+    
+
+        
