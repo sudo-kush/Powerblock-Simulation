20eec71ffe7c96d383d093cc3d6caf6e7e311f41
Caleb Menjivar <54721971+sudo-kush@users.noreply.github.com>
2020-04-24 10:51:58 -0700

    Add files via upload

--- scenario1.py
+++ scenario1.py
@@ -10,36 +10,49 @@ import numpy as np
 import power
 import matplotlib.pyplot as plt 
 
+# -----------------------------------------------------------------------------
+#Things to keep in mind 
 '''
 3 Hour Fill -> max 370 MWth Storage 
 6 Hour Fill -> max 275 MWth Storage 
 9 Hour Fill -> max 200 MWth Storage 
 12 Hour Fill -> max 173 MWth Storage 
 '''
+# -----------------------------------------------------------------------------
+#Initializing variables 
 
-fast = 370 #MW  Most Storage Can Take 
-reactor = 1000 #MW 
-massflow = 450 #kg/s
+fast = 370                                  #MW  Most Storage Can Take 
+reactor = 1000                              #MW 
+massflow = 450                              #kg/s of steam 
+grid = 50                                   #amount of grid points 
 
-x0 = np.linspace(0,100,101) 
-y0 = np.linspace(1,fast,101)
-eff = np.zeros(len(x0))
-P = np.zeros(len(x0))
-Q = np.zeros(len(x0))
+x0 = np.linspace(0,100,grid)                
+y0 = np.linspace(1,fast,grid)               #setting up X 
+eff = np.zeros(len(x0))                     #initializing efficiency array
+P = np.zeros(len(x0))                       #intitializing power array
+Q = np.zeros(len(x0))                       #initializing heat array
+# -----------------------------------------------------------------------------
+#Run through scenarios 
 
 for i in range(len(x0)):
     eff[i], P[i], Q[i] = power.Cycle(1-y0[i]/reactor, massflow)
     eff[i] = eff[i]*100
-    
+# -----------------------------------------------------------------------------
+#Plotting Efficiency vs. Energy Stored 
+
 fig1 = plt.figure()
 plt.plot(y0, eff) 
 plt.xlabel('Amount of Energy put in Storage (MW)')
 plt.ylabel('Steam Plant Efficiency %')
+# -----------------------------------------------------------------------------
+#Plotting Power vs. Energy Stored 
 
 fig2 = plt.figure() 
 plt.plot(y0, P)
-plt.xlabel('Amount of Enerfy put in Storage (MW)')
+plt.xlabel('Amount of Energy put in Storage (MW)')
 plt.ylabel('Power Output (MWe)') 
+# -----------------------------------------------------------------------------
+#Show figures to console 
 
 plt.show(fig1)
 plt.show(fig2)
--- scenario2.py
+++ scenario2.py
@@ -10,59 +10,89 @@ import power
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+# -----------------------------------------------------------------------------
+#Set up Testing Conditions 
+
 temp_low = 575 + 273 #K
 temp_high = 600 + 273 #K
+# -----------------------------------------------------------------------------
+#Get values from storage
 
 a = storage()
 massflow = a.massflow_salt()*-1
 hot_temp = a.hot_temp()
 cold_temp = a.cold_temp()
+# -----------------------------------------------------------------------------
+#Initialize Intermediate Heat Exchanger Massflow 
 
 IHX_massflow = 2400
+# -----------------------------------------------------------------------------
+#Specific Heat for Salt 
 
 specifich_salt = 1520/1000 #KJ/kg
+# -----------------------------------------------------------------------------
+#Set up arrays for the Temperature Diff & Salt Enthalpy Gradient 
 
 temp_grad = np.linspace(temp_low-273, temp_high-273, 15)
 enthalpy_salt = np.linspace(temp_low*specifich_salt, temp_high*specifich_salt, 15)
+# -----------------------------------------------------------------------------
+#Set up arrays for Q into Steam Generator and Combined Enthalpys 
 
 Q_sg = np.zeros(15)
 new_h = np.zeros(15)
+# -----------------------------------------------------------------------------
+#Mass Balance 
 
 new_m = massflow + IHX_massflow 
+# -----------------------------------------------------------------------------
+#Mass Balance with enthalpies 
 
 for i in range(len(new_h)):
     new_h[i] = (IHX_massflow*enthalpy_salt[-1] + massflow*enthalpy_salt[i])/(new_m)
     Q_sg[i] = new_m*(new_h[i] - (cold_temp)*specifich_salt)/1000
 
+# -----------------------------------------------------------------------------
+#Set up Inital Design Conditions for Graphing 
 
 steam_massflow = 500 
 load = np.linspace(0.9,0.95,40)
 tolerance = 5.0
 real_load = np.zeros(15) 
+# -----------------------------------------------------------------------------
+#Run throught iter to find actual load 
 
 for j in range(len(new_h)):
     for k in range(len(load)):
         if (abs(power.CycleQ(steam_massflow,load[k])-Q_sg[j])<tolerance):
             real_load[j] = load[k] 
-            
+# -----------------------------------------------------------------------------            
+#Initialize arrays for Power and Efficiencies 
+
 Power = np.zeros(15)
 nth = np.zeros(15)
 temp = 0
+# -----------------------------------------------------------------------------
+#Iterate through Power Cycle 
 
 for a in range(len(Power)):
     nth[a], Power[a], temp = power.Cycle(real_load[a],steam_massflow)
-    nth[a] = round(nth[a]*100,2)
-    Power[a] = round(Power[a],2)
-    
+    nth[a] = nth[a]*100 
+# -----------------------------------------------------------------------------   
+#Plot TES Exit Temperature vs. Thermal Efficiency     
+
 fig1 = plt.figure()
 plt.plot(temp_grad, nth)
-plt.xlabel('TES Exit Temperature (K)')
+plt.xlabel('TES Exit Temperature (C)')
 plt.ylabel('Thermal Efficiency %')
+# -----------------------------------------------------------------------------
+#Plot TES Exit Temperature vs. Power Output 
 
 fig2 = plt.figure()
 plt.plot(temp_grad, Power)
-plt.xlabel('TES Exit Temperature (K)')
+plt.xlabel('TES Exit Temperature (C)')
 plt.ylabel('Power Output (MWe)')
+# -----------------------------------------------------------------------------
+#Print plots to console 
 
 plt.show(fig1)
 plt.show(fig2)
